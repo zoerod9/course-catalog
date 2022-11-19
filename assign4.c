0a944dc0779c3734ab12e12e7f9654caa5c28ecc
Zoe Rodriguez <zoe.rod9767@gmail.com>
2022-11-18 20:07:43 -0600

    read

--- assign4.c
+++ assign4.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void read();
+
 typedef struct
 {
     char course_Name[80];
@@ -11,48 +13,68 @@ typedef struct
 
 int main()
 {
-    while (1)
-    {
+    read();
+    // while (1)
+    // {
 
-        printf("Enter one of the following actions or press CTRL-D to exit.\n");
-        printf("C - create a new course record\n");
-        printf("U - update an existing course record\n");
-        printf("R - read an existing course record\n");
-        printf("D - delete an existing course record\n");
+    //     printf("Enter one of the following actions or press CTRL-D to exit.\n");
+    //     printf("C - create a new course record\n");
+    //     printf("U - update an existing course record\n");
+    //     printf("R - read an existing course record\n");
+    //     printf("D - delete an existing course record\n");
 
-        char response[1];
-        gets(response);
-        char input = response[0];
+    //     char response[1];
+    //     gets(response);
+    //     char input = response[0];
 
-        printf("your char is %c", input);
+    //     switch (input)
+    //     {
 
-        switch (input)
-        {
+    //     case 'C':
+    //     case 'c':
+    //         printf("you hit c block\n");
+    //         break;
 
-        case 'C':
-        case 'c':
-            printf("you hit c block\n");
-            break;
+    //     case 'U':
+    //     case 'u':
+    //         printf("you hit u block\n");
+    //         break;
 
-        case 'U':
-        case 'u':
-            printf("you hit u block\n");
-            break;
+    //     case 'R':
+    //     case 'r':
+    //         read();
+    //         break;
 
-        case 'R':
-        case 'r':
-            printf("you hit r block\n");
-            break;
+    //     case 'D':
+    //     case 'd':
+    //         printf("you hit d block\n");
+    //         break;
 
-        case 'D':
-        case 'd':
-            printf("you hit d block\n");
-            break;
+    //     default:
+    //         printf("ERROR: invalid option\n");
+    //     }
+    // }
 
-        default:
-            printf("ERROR: invalid option\n");
-        }
-    }
-    
     return 0;
+}
+
+void read()
+{
+    printf("Enter a CS course number:\n");
+    int courseNumber;
+    scanf("%i", &courseNumber);
+
+    FILE *courses = fopen("./courses.dat", "rb");
+
+    Course course;
+    fseek(courses, courseNumber * sizeof(Course), 0);
+    int read = fread(&course, sizeof(Course), 1L, courses);
+
+    printf("Course number: %i\n", courseNumber);
+    printf("Course name: %s\n", course.course_Name);
+    printf("Scheduled days: %s\n", course.course_Sched);
+    printf("Credit hours: %i\n", course.course_Hours);
+    printf("Enrolled Students: %i\n", course.course_Size);
+
+    fclose(courses);
 }
